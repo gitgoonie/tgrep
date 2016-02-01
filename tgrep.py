731d759abf9adc80f75fbce446d00ce3e31cc3cb
gitgoonie <tkriegbaum@web.de>
2016-02-01 08:40:45 +0100

    -m option removed

--- tgrep.py
+++ tgrep.py
@@ -22,18 +22,9 @@ def main():
     import argparse, sys, fileinput
     from optparse import OptionParser
 
-#    parser = argparse.ArgumentParser(description="This program provides search features through paragraphs in text files")
-#    parser.add_argument("searchpattern", help="string to match in the given file")
-#    parser.add_argument("file", help="filename to search through" )
-#    args = parser.parse_args()
-
     parser = OptionParser(usage="Usage: %prog [options] [file, file, ...]")
 #    parser.add_option("-a", "--any", dest="match_any", action="store_true", default=False, help="Match ACLs with 'any', too")
-    parser.add_option("-m", "--match", dest="matchcode", default=None, help="string to be matched")
-#    parser.add_option("-p", "--sport", dest="source_port", default=None, help="Source port to look for")
-#    parser.add_option("-I", "--dip", dest="destination_ip", default=None, help="Destination IP to look for")
-#    parser.add_option("-P", "--dport", dest="destination_port", default=None, help="Destination port to look for")
-#    parser.add_option("-o", "--proto", dest="protocol", default=None, help="Protocol to look for")
+#    parser.add_option("-m", "--match", dest="matchcode", default=None, help="string to be matched")
 
 
     (options, args) = parser.parse_args()
@@ -42,26 +33,15 @@ def main():
         parser.print_help()
         sys.exit()
 
-    match = options.matchcode
-
-    # ...check all lines in all files (or stdin)
-#    for line in fileinput.input(args):
-#        if grepper.grep(line):
-#            print(line.strip())
-
-
-#	ifilename = args.file
-#    match = match
-
-	
-#	ifile = open(ifilename, 'r')
-
+    match = args[0]
 
     output = 0
     i = 0
     n = 0
     para = [ '' ]
 
+    del args[0]     # remove match code and leave files
+    
     for line in fileinput.input(args):
 
         if line[0] !=' ':    # if begin of new paragraph
